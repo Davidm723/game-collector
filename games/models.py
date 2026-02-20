1a572a368e3c72d4acbcca8c830e5ffd339b9cf2
Davidm723 <davidmullokandov@gmail.com>
2026-02-19 21:00:16 -0500

    init commit, models created

---
+++ games/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class Console(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    manufacturer = models.CharField(max_length=100, blank=True)
+    release_year = models.PositiveIntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Game(models.Model):
+    console = models.ForeignKey(Console, on_delete=models.CASCADE, related_name="games")
+    genres = models.ManyToManyField(Genre, related_name="games", blank=True)
+    rawg_id = models.IntegerField(unique=True)
+    title = models.CharField(max_length=200)
+    released = models.DateField(null=True, blank=True)
+    image = models.URLField(blank=True)
+    notes = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
